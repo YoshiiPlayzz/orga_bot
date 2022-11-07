d87cf72badc528a3dd540162df1417c1af143618
YoshiiPlayzz <yoshiiplayzz@gmail.com>
2022-11-07 01:46:25 +0100

    Änderung in start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash 
 echo "Der Bot wird abgeglichen mit dem Remote-Branch und wird falls Änderungen diese herunterladen..."
 git pull
+
+echo "\nDer Bot wird nun gestartet!"
 screen -S "Orga Bot" python main.py
