c9717b580d4ac745a8845da69453cfb69a6af866
YoshiiPlayzz <yoshiiplayzz@gmail.com>
2022-11-07 01:57:45 +0100

    chmod 777 start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 echo "Der Bot wird abgeglichen mit dem Remote-Branch und wird falls Änderungen diese herunterladen..."
 git reset --hard
 git pull
+chmod 777 start.sh
 
-echo "\nDer Bot wird nun gestartet!"
+echo "Der Bot wird nun gestartet!"
 screen -S "Orga Bot" python main.py
