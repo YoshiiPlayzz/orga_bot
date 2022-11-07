565fc0c1c085484db053201f7eb2489abafcd38e
YoshiiPlayzz <yoshiiplayzz@gmail.com>
2022-11-07 19:45:18 +0100

    insert

--- main.py
+++ main.py
@@ -83,8 +83,10 @@ async def getCalendarInformations():
                     if event:
                         time.sleep(2)
                         print(event)
+                        insert = f"INSERT INTO event VALUES('{ev.uid}', '{category}', '{ev.description}', '{ev.begin}', '{ev.end}', '{ev.name}', '{event.get('id')}'"
+                        print(insert)
                         req = cur.execute(
-                            f"INSERT INTO event VALUES('{ev.uid}', '{category}', '{ev.description}', '{ev.begin}', '{ev.end}', '{ev.name}', '{event.get('id')}')"
+                            insert
                         )
                         con.commit ()
 
