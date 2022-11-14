0823aedf11375edf234bbb5bed6906612d5822fc
YoshiiPlayzz <yoshiiplayzz@gmail.com>
2022-11-14 23:42:47 +0100

    Check ob es den Thread gibt

--- quickstart.py
+++ quickstart.py
@@ -9,7 +9,11 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+SCOPES = [
+        "https://www.googleapis.com/auth/docs",
+        "https://www.googleapis.com/auth/drive",
+        "https://www.googleapis.com/auth/drive.metadata.readonly"
+    ]
 
 
 def main():
